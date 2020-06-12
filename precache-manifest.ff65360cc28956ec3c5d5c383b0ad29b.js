d957e191f956b409e732b6ca8da66c9b28c9fee4
vivgarcia <viv.garcia88@gmail.com>
2020-06-11 20:33:12 -0500

    Updates

---
+++ precache-manifest.ff65360cc28956ec3c5d5c383b0ad29b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5113f49bc2a8a29c7300e88f2e4c1812",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "376ed86dd457c57af48d",
+    "url": "/clicky-game/static/css/main.e0c55280.chunk.css"
+  },
+  {
+    "revision": "f632008809298eef02be",
+    "url": "/clicky-game/static/js/2.8445f2d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.8445f2d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376ed86dd457c57af48d",
+    "url": "/clicky-game/static/js/main.daca6c70.chunk.js"
+  },
+  {
+    "revision": "ddea091f236f02df6e9c",
+    "url": "/clicky-game/static/js/runtime-main.de0ba425.js"
+  }
+]);
--- src/components/Sqaure.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Sqaure() {
-    return (
-        
-        <div className="square"></div>
-        
-    )
-}
